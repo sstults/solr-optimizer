be7bca13d40fcfbc415fb2e248a00ae47d2c303c
Scott Stults <sstults@opensourceconnections.com>
2025-05-20 18:41:34 -0400

    Fix Black formatting issues and add .gitattributes for consistent line endings

--- solr_optimizer/__init__.py
+++ solr_optimizer/__init__.py
@@ -1,7 +1,7 @@
 """
 Solr Optimizer - A framework for optimizing Solr queries through systematic experimentation.
 
-This framework provides a set of agents that collaborate to tune and optimize 
+This framework provides a set of agents that collaborate to tune and optimize
 Solr queries based on user-defined relevance metrics.
 """
 
